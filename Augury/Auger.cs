6aae0bac9e1d1d7a7b3151fdf9e4c73ae007fbe4
Jean-Bernard Pellerin <jeanbern@ualberta.ca>
2016-07-17 22:12:11 -0600

    #15 clean up Auger. There was some complicated code that was left over from the past, it is now simplified.

--- Augury/Auger.cs
+++ Augury/Auger.cs
@@ -1,21 +1,16 @@
 ﻿using Augury.Base;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace Augury
 {
-    /// <summary>
-    /// Provides methods for calculating the probability of a word appearing based on words preceeding it. Uses the modified Kneser-Ney algorithm from Chen & Goodman 1999.
-    /// </summary>
-    /// <see cref="http://www2.denizyuret.com/ref/goodman/chen-goodman-99.pdf">Chen & Goodman 1999. The paper detailing the algorithm used here to calculate predictions.</see>
-    /// <seealso cref="http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=479394">Knesser & Ney 1995. The paper referenced by Chen & Goodman</seealso>
-    /// <seealso cref="http://west.uni-koblenz.de/sites/default/files/BachelorArbeit_MartinKoerner.pdf">Martin Christian Körner 2013. For more readable notation.</seealso>
     public class Auger
     {
-        public IPrefixLookup SpellChecker { get; protected set; }
-        public ILanguageModel LanguageModel { get; protected set; }
-        public INextWordModel NextWordModel { get; protected set; }
+        public IPrefixLookup SpellChecker { get; }
+        public ILanguageModel LanguageModel { get; }
+        public INextWordModel NextWordModel { get; }
 
         public Auger(IPrefixLookup spellchecker, ILanguageModel languageModel, INextWordModel nextWordModel)
         {
@@ -25,7 +20,6 @@ namespace Augury
         }
 
         //The larger this value, the more words we check for history probability.
-        //We maintain a sorted list with this length, it could use a lot of processing power.
         private const int MaxSpellCheckResults = 100;
         //This does not change the amount of time it takes to process results.
         private const int MaxResults = 10;
@@ -41,87 +35,51 @@ namespace Augury
         /// <remarks>
         /// If the last string in history is empty, it will attempt to predict entirely new words.
         /// If the last string is not empty, it will use that as the basis for a spell-checked prediction.
-        /// The <see cref="SpellCheck">spell-checker</see> will attempt to provide words which are either close to the provided word, or contain something close as a prefix.
-        /// Closeness of two words is defined by the <see cref="JaroWinkler">Jaro-Winkler</see> distance.
-        /// This distance is estimated for words values below a threshold.
         /// </remarks>
         public IReadOnlyList<string> Predict(IReadOnlyList<string> history)
         {
-            var lastWord = history[history.Count - 1];
-            history = history.Select(x => x.ToLowerInvariant()).ToList();
-            var hLen = history.Count;
-            //Only care about the last 2, since we deal with 3-grams
-            string[] previous;
-            string partial;
-            switch (hLen)
+            var lastIndex = history.Count - 1;
+            if (lastIndex < 0)
             {
-                case 0:
-                    return new List<string>();
-                case 1:
-                    //return SpellingPredictor.PrefixLookup(history[0], MaxSpellCheckResults).OrderByDescending(x => x.Similarity).Take(MaxResults).Select(x => x.Word).ToList();
-                    partial = history[0];
-                    previous = new string[0];
-                    break;
-                case 2:
-                    partial = history[1];
-                    previous = new[] {history[0]};
-                    break;
-                case 3:
-                    partial = history[2];
-                    previous = new[] {history[0], history[1]};
-                    break;
-                default:
-                    partial = history[hLen - 1];
-                    previous = new[] {history[hLen - 3], history[hLen - 2]};
-                    break;
+                return new List<string>();
             }
 
+            var lastWord = history[lastIndex];
+            history = history.Select(x => x.ToLowerInvariant()).ToList();
+
+            var partial = history[lastIndex];
+            //Only care about the last 2, since we deal with 3-grams
+            var previousCount = Math.Max(lastIndex, 2);
+            var previous = history.Skip(history.Count - previousCount).ToArray();
+            
             if (string.IsNullOrWhiteSpace(partial))
             {
                 return NextWordModel.NextWord(previous);
             }
 
-            //This is a (potentially) really huge list of words that are obtained via correcting and/or starting with partial.
+            //Words obtained via correcting and/or starting with partial.
             //Does not care about the word history.
             var spellchecks = SpellChecker.PrefixLookup(partial, MaxSpellCheckResults);
+
+            var possibleWordValue = new string[previousCount + 1];
+            Array.Copy(previous, possibleWordValue, previousCount);
 
             var possibleSpellchecks = new Dictionary<string, double>();
             //Now we take our possible spell-checked words and see if any of them fit well with the history.
             foreach (var spellcheckPair in spellchecks)
             {
-                var spellcheck = spellcheckPair.Word;
-                var distance = spellcheckPair.Similarity;
-                var newList = previous.ToList();
-                newList.Add(spellcheck);
-                var value = LanguageModel.Evaluate(newList.ToArray());
+                possibleWordValue[previousCount] = spellcheckPair.Word;
+                var value = LanguageModel.Evaluate(possibleWordValue);
 
-                possibleSpellchecks.Add(spellcheck, value > 0 ? value*distance : distance - 1);
+                possibleSpellchecks.Add(spellcheckPair.Word, value > 0 ? value*spellcheckPair.Similarity : spellcheckPair.Similarity - 1);
             }
-
-            var ret = possibleSpellchecks.OrderByDescending(x => x.Value).Select(x => x.Key).Take(MaxResults).ToList();
-
-            if (possibleSpellchecks.ContainsKey(partial))
-            {
-                if (ret.Contains(partial))
-                {
-                    ret.Remove(partial);
-                }
-                else
-                {
-                    ret.RemoveAt(ret.Count - 1);
-                }
-
-                if (ret.Count > 0)
-                {
-                    ret.Insert(0, partial);
-                }
-                else
-                {
-                    ret.Add(partial);
-                }
-            }
-
-            return ret.Select(x => Tokenizer.CapitalizeFromTemplate(lastWord, x, hLen == 1)).ToList();
+            
+            return possibleSpellchecks
+                .OrderByDescending(x => x.Value)
+                .Select(x => x.Key)
+                .Take(MaxResults)
+                .Select(x => Tokenizer.CapitalizeFromTemplate(lastWord, x, lastIndex == 0))
+                .ToList();
         }
 
         #region Save
@@ -144,5 +102,29 @@ namespace Augury
         }
 
         #endregion
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Auger;
+            return other != null && Equals(other);
+        }
+
+        protected bool Equals(Auger other)
+        {
+            return Equals(SpellChecker, other.SpellChecker) &&
+                   Equals(LanguageModel, other.LanguageModel) &&
+                   Equals(NextWordModel, other.NextWordModel);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = SpellChecker?.GetHashCode() ?? 0;
+                hashCode = (hashCode*397) ^ (LanguageModel?.GetHashCode() ?? 0);
+                hashCode = (hashCode*397) ^ (NextWordModel?.GetHashCode() ?? 0);
+                return hashCode;
+            }
+        }
     }
 }
